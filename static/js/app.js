d6c11beae7ef9063841086ce56d328d686494f1a
Skander <skander.kc@gmail.com>
2016-05-21 19:56:37 -0400

    Sweetalert login button redirect to G+

--- static/js/app.js
+++ static/js/app.js
@@ -1,7 +1,16 @@
 $(document).ready(function (){
 
   $("#btn-login").click(function(){
-      swal("Test");
+    swal({
+      title: "Connect with Google",
+      text: "You will be redirected to authorize Facture to connect with your Google account.",
+      imageUrl: "/static/images/logo_gplus.png",
+      confirmButtonText: "redirect me",
+      confirmButtonColor: "#DC4A38",
+      showCancelButton: true
+    }, function(){
+      window.location.href = '/login';
+    });
   });
 
 });
