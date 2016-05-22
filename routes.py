aa2ff1da51b1b6d36f367fce78c160e56d79d57b
Skander <skander.kc@gmail.com>
2016-05-22 02:41:58 -0400

    Create different templates

--- routes.py
+++ routes.py
@@ -9,3 +9,7 @@ def index():
         user = User.query.filter_by(id = u_id).first()
         return render_template('home.html', user=user)
     return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
